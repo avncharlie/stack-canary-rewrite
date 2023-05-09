837d7416e47c0b3a3fb06f9a3b9cd91bb230f01c
avncharlie <alvinjoycharles@gmail.com>
2023-05-09 14:43:31 +1000

    Added script that can add stack canary to executables in a few different ways.

--- main.c
+++ main.c
@@ -1,13 +1,30 @@
+//#include <stdio.h>
+//
+//void foo() {
+//    printf("ello mate\n");
+//}
+//
+//int main() {
+//    char str[5];
+//
+//    printf("hello! enter something: ");
+//
+//    scanf("%s", str);
+//
+//    printf("\nyou entered: %s\n", str);
+//
+//}
+
 #include <stdio.h>
 
-int main() {
+void foo() {
     char str[5];
-
     printf("hello! enter something: ");
-
     scanf("%s", str);
-
     printf("\nyou entered: %s\n", str);
+}
 
+int main() {
+    foo();
 }
 
